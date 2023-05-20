36986df256b53a663e0e191cbcf455717e5eaa1a
andenism <kijanghitamm@gmail.com>
2023-05-20 09:35:54 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, exercitationem?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 
 </html>
